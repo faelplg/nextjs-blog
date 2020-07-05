3fe794b6a4688b7aea97793b301cce491e6b6793
Rafael Goulart <faelplg@gmail.com>
2020-07-05 00:05:34 -0300

    Adding a layout component.

---
+++ components/layout.js
@@ -0,0 +1,5 @@
+import styles from './layout.module.css';
+
+const Layout = ({children}) => <div className={styles.container}>{children}</div>;
+
+export default Layout;
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,9 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import Layout from '../../components/layout';
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First Post</title>
       </Head>
@@ -12,6 +13,6 @@ export default function FirstPost() {
           <a>Back to home</a>
         </Link>
       </h2>
-    </>
+    </Layout>
   );
 }
