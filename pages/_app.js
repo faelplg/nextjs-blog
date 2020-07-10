255056719bb817fc3cb1d602f79658633dd74421
Rafael Goulart <faelplg@gmail.com>
2020-07-10 03:18:13 -0300

    Assets, Metadata, and CSS.

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,6 @@
 import '../styles/global.css';
 
 const App = ({Component, pageProps}) => {
-  console.log('DEBUG: on [App].() - Component', Component);
-  console.log('pageProps', pageProps);
   return <Component {...pageProps} />;
 };
 
